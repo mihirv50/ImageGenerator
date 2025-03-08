ca89ee9ed9bf66dff54be3ca02b7ebb6917e8f10
Mihir Vyas <mihirv50@gmail.com>
2025-03-08 15:52:55 +0530

    Changes in button props

--- src/Components/ui/Button.tsx
+++ src/Components/ui/Button.tsx
@@ -7,23 +7,29 @@ interface ButtonProps {
   variant: Variants;
   size: "sm" | "md" | "lg";
   startIcon?: ReactElement;
+  text: string;
 }
 const variantStyles = {
-  primary: "bg-blue-500 text-white",
-  secondary: "bg-green-500 text-white",
+  primary: "bg-blue-600 text-white rounded-md",
+  secondary: "bg-blue-400 text-white rounded-full",
 };
 const sizeStyles = {
-  sm: "py-2 px-2 text-sm rounded-sm",
-  md: "py-4 px-4 text-md rounded-md",
-  lg: "py-6 px-6 text-lg rounded-lg",
+  sm: "py-2 px-2 text-sm",
+  md: "py-4 px-4 text-md ",
+  lg: "py-6 px-6 text-lg",
 };
+const defaultStyles = " flex items-center justify-center gap-2";
+
+const shapeStyle = "rounded-3xl outline-none"
 const Button = (props: ButtonProps) => {
   return (
     <div>
       <button
-        className={`${variantStyles[props.variant]} ${sizeStyles[props.size]}`}
+        className={`${variantStyles[props.variant]} ${
+          sizeStyles[props.size]
+        } ${defaultStyles} ${shapeStyle}`}
       >
-        {props.startIcon} Generate Image
+        {props.startIcon} {props.text}
       </button>
     </div>
   );
